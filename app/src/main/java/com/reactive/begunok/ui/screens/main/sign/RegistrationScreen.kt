cd1df8e111ea616010ef57424c2c9360d6ed52b2
Leyla2627 <leyla2627>
2020-08-06 14:20:45 +0300

    fix logIn

--- app/src/main/java/com/reactive/begunok/ui/screens/main/sign/RegistrationScreen.kt
+++ app/src/main/java/com/reactive/begunok/ui/screens/main/sign/RegistrationScreen.kt
@@ -1,15 +1,16 @@
 package com.reactive.begunok.ui.screens.main.sign
 
-import android.text.InputType
 import com.reactive.begunok.R
 import com.reactive.begunok.base.BaseFragment
+
 
 class RegistrationScreen : BaseFragment(R.layout.screen_registration) {
 
     override fun initialize() {
 
-//        edtTxt.setInputType(  InputType.TYPE_TEXT_VARIATION_PASSWORD)
-//        edtTxt.setSelection(edtTxt.length())
+
     }
+
+
 
 }
