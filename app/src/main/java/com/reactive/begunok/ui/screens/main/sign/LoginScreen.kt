dbc71ec0b93c0294c9d1b30a3263e6265d9f7f3e
Leyla2627 <leyla2627>
2020-08-07 08:39:45 +0300

    login, Registration fragment

--- app/src/main/java/com/reactive/begunok/ui/screens/main/sign/LoginScreen.kt
+++ app/src/main/java/com/reactive/begunok/ui/screens/main/sign/LoginScreen.kt
@@ -1,14 +1,50 @@
 package com.reactive.begunok.ui.screens.main.sign
 
+import android.util.Patterns
 import com.reactive.begunok.R
 import com.reactive.begunok.base.BaseFragment
+import com.reactive.begunok.utils.common.TextObservable
+import com.reactive.begunok.utils.extensions.blockClickable
+import com.reactive.begunok.utils.extensions.inDevelopment
+import kotlinx.android.synthetic.main.screen_login.*
+import kotlinx.android.synthetic.main.screen_login.email
+import kotlinx.android.synthetic.main.screen_login.passw
 
 class LoginScreen : BaseFragment(R.layout.screen_login) {
 
     override fun initialize() {
+        email.addTextChangedListener(TextObservable(enter) {})
+        passw.addTextChangedListener(TextObservable(enter) {})
 
-//        edtTxt.setInputType(  InputType.TYPE_TEXT_VARIATION_PASSWORD)
-//        edtTxt.setSelection(edtTxt.length())
+        enter.setOnClickListener {
+
+            if (passw.text.toString().length < 6) {
+                passw.error = getString(R.string.minimum_password_length_6)
+                return@setOnClickListener
+            }
+            if (passw.text.toString().length > 120) {
+                passw.error = getString(R.string.maximum_password_length_120)
+                return@setOnClickListener
+            }
+
+            var emailAdress: String? = null
+            emailAdress = email.text.toString()
+
+            if (!(emailAdress.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailAdress).matches())) {
+                email.error = getString(R.string.the_verified_password_is_not_valid)
+                return@setOnClickListener
+            }
+
+
+
+            it.blockClickable()
+        }
+        failed.setOnClickListener {  }
+
+
+
+
+
     }
 
 }
