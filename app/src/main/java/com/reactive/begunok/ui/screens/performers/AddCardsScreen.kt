5ebe997e9b68cbbbaaa6f8da90078cd52455b6df
Abdulloh <maxmudovabdullo97@gmail.com>
2020-09-09 02:32:52 +0500

    credit cards screen

--- app/src/main/java/com/reactive/begunok/ui/screens/performers/AddCardsScreen.kt
+++ app/src/main/java/com/reactive/begunok/ui/screens/performers/AddCardsScreen.kt
@@ -5,7 +5,6 @@ import com.reactive.begunok.base.BaseFragment
 import com.reactive.begunok.utils.extensions.inDevelopment
 import kotlinx.android.synthetic.main.content_header.*
 import kotlinx.android.synthetic.main.screen_add_cards_performer.*
-import kotlinx.android.synthetic.main.screen_add_cards_performer.*
 
 
 class AddCardsScreen : BaseFragment(R.layout.screen_add_cards_performer) {
--- app/src/main/java/com/reactive/begunok/ui/screens/performers/RemoveCardsScreen.kt
+++ app/src/main/java/com/reactive/begunok/ui/screens/performers/RemoveCardsScreen.kt
@@ -1,11 +1,14 @@
 package com.reactive.begunok.ui.screens.performers
 
+import androidx.core.app.ActivityOptionsCompat
+import androidx.lifecycle.MutableLiveData
+import com.amyu.stack_card_layout_manager.StackCardLayoutManager
 import com.reactive.begunok.R
 import com.reactive.begunok.base.BaseFragment
+import com.reactive.begunok.ui.screens.performers.cards.CardViewModel
+import com.reactive.begunok.ui.screens.performers.cards.StackCardAdapter
 import com.reactive.begunok.utils.extensions.inDevelopment
 import kotlinx.android.synthetic.main.content_header.*
-import kotlinx.android.synthetic.main.screen_add_cards_performer.*
-import kotlinx.android.synthetic.main.screen_remove_cards_performer.save
 import kotlinx.android.synthetic.main.screen_remove_cards_performer.*
 
 class RemoveCardsScreen : BaseFragment(R.layout.screen_remove_cards_performer) {
@@ -26,11 +29,51 @@ class RemoveCardsScreen : BaseFragment(R.layout.screen_remove_cards_performer) {
         header.text = "Удалить платежную карту"
 
 
-        val images: ArrayList<Int> = ArrayList()
-        images.add(R.drawable.card)
-        images.add(R.drawable.card)
-        images.add(R.drawable.card)
-        sliderViewRemove.setImages(images)
+        val adapter = StackCardAdapter(requireContext()).apply {
+            onItemClickListener = { cardView, _ ->
+              //  val compat = ActivityOptionsCompat.makeSceneTransitionAnimation(this@RemoveCardsScreen, cardView, cardView.transitionName)
+               // startActivityWithOptions(compat) { DetailActivity.createIntent(it, cardViewModel.title.value!!, cardViewModel.background.value!!, cardViewModel.logo.value!!) }
+            }
+            submitList(
+                listOf(
+                    CardViewModel(
+                        title = MutableLiveData<String>().apply {
+                            postValue("Master card")
+                        },
+                        background = MutableLiveData<Int>().apply {
+                            postValue(R.drawable.backgroundd)
+                        },
+                        logo = MutableLiveData<Int>().apply {
+                            postValue(R.drawable.mastercard)
+                        }
+                    ),
+                    CardViewModel(
+                        title = MutableLiveData<String>().apply {
+                            postValue("Master card")
+                        },
+                        background = MutableLiveData<Int>().apply {
+                            postValue(R.drawable.backgroundd)
+                        },
+                        logo = MutableLiveData<Int>().apply {
+                            postValue(R.drawable.mastercard)
+                        }
+                    ),
+                    CardViewModel(
+                        title = MutableLiveData<String>().apply {
+                            postValue("Visa")
+                        },
+                        background = MutableLiveData<Int>().apply {
+                            postValue(R.drawable.backgroundd)
+                        },
+                        logo = MutableLiveData<Int>().apply {
+                            postValue(R.drawable.visa)
+                        }
+                    )
+                )
+            )
+        }
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = StackCardLayoutManager(5)
     }
 
 }
