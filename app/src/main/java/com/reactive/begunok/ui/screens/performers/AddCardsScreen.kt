3128931caf5ac9458acd67e4ac10fc0b1293a123
Admin <arab1997>
2020-09-09 19:02:57 +0500

    fix code

--- app/src/main/java/com/reactive/begunok/ui/screens/performers/AddCardsScreen.kt
+++ app/src/main/java/com/reactive/begunok/ui/screens/performers/AddCardsScreen.kt
@@ -1,8 +1,7 @@
 package com.reactive.begunok.ui.screens.performers
 
-import android.util.Log
-import android.view.View
-import com.fevziomurtekin.payview.data.PayModel
+import android.text.InputType
+import com.braintreepayments.cardform.view.CardForm
 import com.reactive.begunok.R
 import com.reactive.begunok.base.BaseFragment
 import com.reactive.begunok.utils.extensions.inDevelopment
@@ -15,7 +14,6 @@ class AddCardsScreen : BaseFragment(R.layout.screen_add_cards_performer) {
     override fun initialize() {
         initClicks()
         initViews()
-
     }
 
     private fun initClicks() {
@@ -25,29 +23,6 @@ class AddCardsScreen : BaseFragment(R.layout.screen_add_cards_performer) {
 
     private fun initViews() {
         header.text = "Добавить платежную карту"
-
-
-       // val creditCardView = CreditCardView(context)
-
-        val name = "HARISH SRIDHARAN"
-        val cvv = "522"
-        val expiry = "01/17"
-        val cardNumber = "38056789000000000"
-
-
-        payview.setOnDataChangedListener(object : com.fevziomurtekin.payview.Payview.OnChangelistener{
-            override fun onChangelistener(payModel: PayModel?, isFillAllComponents: Boolean) {
-                Log.d("PayView", "data : ${payModel?.cardOwnerName} \n " +
-                        "is Fill all form component : $isFillAllComponents")
-
-            }
-
-        })
-
-        payview.setPayOnclickListener(View.OnClickListener {
-            Log.d("PayView "," clicked. iss Fill all form Component : ${payview.isFillAllComponents}")
-
-        })
 
 
     }
